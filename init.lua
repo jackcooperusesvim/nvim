9fc32b35a124643b0b073fa0057691762646343f
jackcooperusesvim <andreft316@gmail.com>
2024-10-22 17:00:37 -0400

    floaterm

--- init.lua
+++ init.lua
@@ -137,7 +137,7 @@ vim.opt.updatetime = 250
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
 vim.opt.timeoutlen = 300
-vim.opt.colorcolumn = '79'
+-- vim.opt.colorcolumn = '79'
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
@@ -376,7 +376,7 @@ require('lazy').setup({
         -- defaults = {
         --   mappings = {
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
+        --   },.
         -- },
         -- pickers = {}
         extensions = {
@@ -790,7 +790,7 @@ require('lazy').setup({
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
-  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+  { 'EdenEast/nightfox.nvim', lazy = false, priority = 1000 },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
   { 'windwp/nvim-ts-autotag', lazy = false },
@@ -1162,62 +1162,60 @@ end)
 vim.keymap.set('n', '<C-S-N>', function()
   harpoon:list():next()
 end)
-require('catppuccin').setup {
-  flavour = 'mocha', -- latte, frappe, macchiato, mocha
-  background = { -- :h background
-    light = 'latte',
-    dark = 'mocha',
-  },
-  transparent_background = true, -- disables setting the background color.
-  show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-  term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-  dim_inactive = {
-    enabled = false, -- dims the background color of inactive window
-    shade = 'dark',
-    percentage = 0.15, -- percentage of the shade to apply to the inactive window
-  },
-  no_italic = false, -- Force no italic
-  no_bold = false, -- Force no bold
-  no_underline = false, -- Force no underline
-  styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-    comments = { 'italic' }, -- Change the style of comments
-    conditionals = { 'italic' },
-    loops = {},
-    functions = {},
-    keywords = {},
-    strings = {},
-    variables = {},
-    numbers = {},
-    booleans = {},
-    properties = {},
-    types = { 'altfont' },
-    operators = {},
-    -- miscs = {}, -- Uncomment to turn off hard-coded styles
-  },
-  color_overrides = {},
-  custom_highlights = {},
-  default_integrations = true,
-  integrations = {
-    cmp = true,
-    gitsigns = true,
-    nvimtree = true,
-    treesitter = true,
-    notify = false,
-    mini = {
-      enabled = true,
-      indentscope_color = '',
-    },
-    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-  },
-}
 
+-- Default options
+require('nightfox').setup {
+  options = {
+    -- Compiled file's destination location
+    compile_path = vim.fn.stdpath 'cache' .. '/nightfox',
+    compile_file_suffix = '_compiled', -- Compiled file suffix
+    transparent = false, -- Disable setting background
+    terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+    dim_inactive = false, -- Non focused panes set to alternative background
+    module_default = true, -- Default enable value for modules
+    colorblind = {
+      enable = false, -- Enable colorblind support
+      simulate_only = false, -- Only show simulated colorblind colors and not diff shifted
+      severity = {
+        protan = 0, -- Severity [0,1] for protan (red)
+        deutan = 0, -- Severity [0,1] for deutan (green)
+        tritan = 0, -- Severity [0,1] for tritan (blue)
+      },
+    },
+    styles = { -- Style to be applied to different syntax groups
+      comments = 'NONE', -- Value is any valid attr-list value `:help attr-list`
+      conditionals = 'NONE',
+      constants = 'NONE',
+      functions = 'NONE',
+      keywords = 'NONE',
+      numbers = 'NONE',
+      operators = 'NONE',
+      strings = 'NONE',
+      types = 'NONE',
+      variables = 'NONE',
+    },
+    inverse = { -- Inverse highlight for different types
+      match_paren = false,
+      visual = false,
+      search = false,
+    },
+    modules = { -- List of various plugins and additional options
+      -- ...
+    },
+  },
+  palettes = {},
+  specs = {},
+  groups = {},
+}
 -- setup must be called before loading
-vim.cmd.colorscheme 'catppuccin'
+
+vim.cmd.colorscheme 'carbonfox'
 
 -- vim.keymap.set('n', '<C-f>', '<C-f>zz')
 -- vim.keymap.set('n', '<C-b>', '<C-b>zz')
 vim.keymap.set('n', '<leader>cm', '<cmd>TransparentToggle<CR>')
-vim.cmd 'TransparentToggle'
+vim.keymap.set({ 'n', 'v', 'i' }, '<F15>', '<cmd>FloatermToggle<CR>')
+--TODO: BUILD A PYTHON THING TO GO HERE
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/plugins/floaterm.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    'voldikss/vim-floaterm',
+    cmd = { 'FloatermToggle', 'FloatermNew', 'FloatermPrev', 'FloatermNext' },
+    config = function() end,
+    opts = {},
+  },
+}
