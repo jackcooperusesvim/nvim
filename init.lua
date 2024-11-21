6583528d1a2ffca65bf809abd690237d7fa54d5a
jackcooperusesvim <andreft316@gmail.com>
2024-11-20 20:52:04 -0500

    gruvbox and lualine

--- init.lua
+++ init.lua
@@ -790,7 +790,7 @@ require('lazy').setup({
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
-  { 'EdenEast/nightfox.nvim', lazy = false, priority = 1000 },
+  { 'ellisonleao/gruvbox.nvim', lazy = false, priority = 1000 },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
   { 'windwp/nvim-ts-autotag', lazy = false },
@@ -1164,52 +1164,13 @@ vim.keymap.set('n', '<C-S-N>', function()
 end)
 
 -- Default options
-require('nightfox').setup {
-  options = {
-    -- Compiled file's destination location
-    compile_path = vim.fn.stdpath 'cache' .. '/nightfox',
-    compile_file_suffix = '_compiled', -- Compiled file suffix
-    transparent = false, -- Disable setting background
-    terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-    dim_inactive = false, -- Non focused panes set to alternative background
-    module_default = true, -- Default enable value for modules
-    colorblind = {
-      enable = false, -- Enable colorblind support
-      simulate_only = false, -- Only show simulated colorblind colors and not diff shifted
-      severity = {
-        protan = 0, -- Severity [0,1] for protan (red)
-        deutan = 0, -- Severity [0,1] for deutan (green)
-        tritan = 0, -- Severity [0,1] for tritan (blue)
-      },
-    },
-    styles = { -- Style to be applied to different syntax groups
-      comments = 'NONE', -- Value is any valid attr-list value `:help attr-list`
-      conditionals = 'NONE',
-      constants = 'NONE',
-      functions = 'NONE',
-      keywords = 'NONE',
-      numbers = 'NONE',
-      operators = 'NONE',
-      strings = 'NONE',
-      types = 'NONE',
-      variables = 'NONE',
-    },
-    inverse = { -- Inverse highlight for different types
-      match_paren = false,
-      visual = false,
-      search = false,
-    },
-    modules = { -- List of various plugins and additional options
-      -- ...
-    },
-  },
-  palettes = {},
-  specs = {},
-  groups = {},
-}
+require("gruvbox").setup({
+  terminal_colors = true,
+  transparent_mode = true
+})
 -- setup must be called before loading
 
-vim.cmd.colorscheme 'carbonfox'
+vim.cmd.colorscheme 'gruvbox'
 
 -- vim.keymap.set('n', '<C-f>', '<C-f>zz')
 -- vim.keymap.set('n', '<C-b>', '<C-b>zz')
