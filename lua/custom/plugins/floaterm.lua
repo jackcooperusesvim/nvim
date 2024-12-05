57570dc081ac78a8320d17bd381cf6e5124d80ac
jackcooperusesvim <andreft316@gmail.com>
2024-12-05 12:43:10 -0500

    hard reset here

--- lua/custom/plugins/floaterm.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    'voldikss/vim-floaterm',
-    cmd = { 'FloatermToggle', 'FloatermNew', 'FloatermPrev', 'FloatermNext', 'FloatermSend' },
-    config = function() end,
-    opts = {},
-  },
-}
--- lua/custom/plugins/wc.lua
+++
@@ -1,1 +0,0 @@
-return { 'dimfeld/section-wordcount.nvim' }
