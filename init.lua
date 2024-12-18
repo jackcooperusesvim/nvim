a0757201b8bd0f7ed982739f1bdbd43aba28becb
jackcooperusesvim <andreft316@gmail.com>
2024-12-18 15:50:24 -0500

    asdfasdf

--- init.lua
+++ init.lua
@@ -194,6 +194,11 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+function split_keep_scroll() end
+-- vim.keymap.set('n', '', '<CMD>vsplit<CR>', { desc = 'Move focus to the upper window' })
+
+-- TODO: FINISH THIS
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
